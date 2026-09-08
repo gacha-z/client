8a43c46de0ce86b315dd2dc7f2706303a5ef8e97
Kimd0ng <kdongh0406@gmail.com>
2026-09-08 22:53:36 +0900

    [feat] #20 packages/api 배럴에 신규 쿼리 export 추가

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -5,6 +5,37 @@ export {
   createApiClient,
   getApiClient,
   isApiError,
-  setAuthTokenProvider
+  setAuthTokenProvider,
+  unwrap,
+  type ApiEnvelope
 } from './client';
 export { exampleQueryKey, exampleQueryOptions, type ExampleResponse } from './queries/example';
+export {
+  currentMemberQueryKey,
+  currentMemberQueryOptions,
+  type CurrentMember
+} from './queries/member';
+export {
+  tripDetailQueryKey,
+  tripDetailQueryOptions,
+  tripMembersQueryKey,
+  tripMembersQueryOptions
+} from './queries/trip';
+export {
+  missionCandidatesQueryKey,
+  missionCandidatesQueryOptions,
+  selectMissionCandidate,
+  rerollMissionCandidate,
+  completeMission,
+  failMission,
+  type SelectMissionCandidateParams,
+  type RerollMissionCandidateParams,
+  type CompleteMissionParams,
+  type FailMissionParams
+} from './queries/mission';
+export {
+  missionSetlogsQueryKey,
+  missionSetlogsQueryOptions,
+  uploadSetlog,
+  type UploadSetlogParams
+} from './queries/setlog';
