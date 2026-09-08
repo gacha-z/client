d0feec76de94fa361bf8247a1795746dc05f3fb7
Kimd0ng <kdongh0406@gmail.com>
2026-09-08 23:03:15 +0900

    [refactor] #20 미션 mock atom 제거, API 연동에 필요한 최소 atom만 유지

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -21,25 +21,7 @@ export {
   travelCreationAtom,
   type TravelCreationState
 } from './atoms/travelCreate';
-export {
-  MOCK_MEMBERS,
-  MOCK_TRIP,
-  MOCK_TODAY_PROGRESS,
-  missionStageAtom,
-  missionOutcomesAtom,
-  todayCompletedMissionCountAtom,
-  dailyMissionLimitReachedAtom,
-  missionCandidatesAtom,
-  selectedMissionAtom,
-  memberVerificationsAtom,
-  allMembersVerifiedAtom,
-  startMissionSelectionAtom,
-  retryCandidateAtom,
-  selectMissionAtom,
-  verifyMemberAtom,
-  giveUpMissionAtom,
-  clearMissionAtom
-} from './atoms/mission';
+export { activeMissionAtom, missionOutcomesAtom, missionStageAtom } from './atoms/mission';
 export { createQueryClient } from './query-client';
 export { AppProviders } from './providers';
 export type { MockUserProfile } from './mocks/user';
