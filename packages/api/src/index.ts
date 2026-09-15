df038e2d34ca254215db1c4b176d459274de80a0
Kimd0ng <kdongh0406@gmail.com>
2026-09-16 01:32:57 +0900

    [feat] #27 셋로그/미션이력 API 레이어 추가

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -59,6 +59,8 @@ export {
 export {
   missionCandidatesQueryKey,
   missionCandidatesQueryOptions,
+  missionHistoryQueryKey,
+  missionHistoryQueryOptions,
   selectMissionCandidate,
   rerollMissionCandidate,
   completeMission,
@@ -71,6 +73,10 @@ export {
 export {
   missionSetlogsQueryKey,
   missionSetlogsQueryOptions,
+  tripSetlogsQueryKey,
+  tripSetlogsQueryOptions,
   uploadSetlog,
-  type UploadSetlogParams
+  downloadSetlog,
+  type UploadSetlogParams,
+  type TripSetlogsParams
 } from './queries/setlog';
